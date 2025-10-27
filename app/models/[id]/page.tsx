37af69912e61a3da27d53b6c058d6fff67f2e3ed
Makr <534640202@qq.com>
2025-10-27 14:48:48 +0800

    1234

--- app/models/[id]/page.tsx
+++ app/models/[id]/page.tsx
@@ -3,6 +3,9 @@ import { notFound } from 'next/navigation'
 import { createServiceClient } from '@/lib/supabase'
 import ModelDetailClient from './client'
 
+// 强制页面动态渲染
+export const dynamic = 'force-dynamic'
+
 interface PageProps {
   params: Promise<{ id: string }>
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
